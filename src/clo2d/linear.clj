83aa8af74380506ab72d2ec4cf43e9cefe403437
Sylvain Leroux <sylvain@chicoree.fr>
2013-09-16 22:14:59 +0200

    First working version of the linear solver

--- src/clo2d/linear.clj
+++ src/clo2d/linear.clj
@@ -8,6 +8,26 @@
   [ ^clojure.lang.IFn f a b ]
   `(map #(~f %1 %2) ~a ~b))
 
+(defn accumulate
+  "Given a vector and the first root values
+  returns the sum v(i)*r(i) and any unused v(j) terms"
+  ( [ vector roots ] (accumulate vector roots 0))
+  ( [ vector roots result ]
+    (let [vi (first vector)
+          ri (first roots)]
+      (if ri
+        (recur (rest vector) (rest roots) (+ (* vi ri) result))
+        (cons result vector)))))
+
+
+(def ^:const +epsilon 1e-5)
+(def ^:const -epsilon (- +epsilon))
+
+(defmacro fzero?
+  "Test if a number is close to zero"
+  ( [ x ] `(fzero? ~x -epsilon +epsilon))
+  ( [ x epsilon] `(fzero? ~x (- ~epsilon) ~epsilon))
+  ( [ x -epsilon +epsilon ] `(< ~-epsilon ~x ~+epsilon)))
 
 ;;
 ;; Gauss solver
@@ -33,7 +53,33 @@
         pivot  (fn [eq]
                  (let [h (first eq) 
                        t (rest eq)
-                       c (/ factor h)]
-                    (product #(- (* %2 c) %1) vec t)))]
+                       c (/ h factor)]
+                    (product #(- %2 (* %1 c)) vec t)))]
     (map pivot eqs)))
+
+(defn pivot
+  ( [ eqs ] (pivot eqs '()))
+  (
+  [ eqs result ]
+  (let [[eq bag] (reorder eqs)]
+    (if eq
+      (let [ rr (row-reduce eq bag) ]
+        (recur rr (cons eq result)))
+      result))))
+
+(defn solve
+  [ eqs ]
+  (let [p (pivot eqs)
+        [ ck ] (first p)]
+    (when (not (fzero? ck))
+      (throw "Can't solve"))
+    (loop [ roots '()
+            eqs (rest p) ]
+      (let [ eq (first eqs) ]
+        (if eq
+          (let [ eqs (rest eqs)
+                 a (first eq)
+                 [s r] (accumulate (rest eq) roots) ]
+            (recur (cons (/ (- r s) a) roots) eqs))
+          roots )))))
 
--- test/clo2d/linear_test.clj
+++ test/clo2d/linear_test.clj
@@ -4,24 +4,53 @@
             [clojure.java.io :refer :all])
   (:import (java.io File)))
 
-(deftest utilities-test
+(deftest product-test
   (testing "Product"
     (let [a [1 2 3]
           b [4 5 6]
           c (product * a b)]
       (is (= [4 10 18] c)))))
 
+(deftest accumulate-test
+  (testing "Accumulate"
+    (let [ vec   '(1 2 3 4)
+           roots '(5 6 7)
+           acc   (accumulate vec roots) ]
+      (is (= '(38 4) acc))))) 
+
+(deftest zero?-test
+  (testing "fzero?"
+    (is (fzero? (/ +epsilon 2)))
+    (is (fzero? (/ +epsilon -2)))
+    (is (fzero? 0.5 1))
+    (is (fzero? -0.5 -1 0.1)) ))
+
+
 (deftest gauss-solver-test
   (testing "Equation reordering"
-    (let [ eqs [[1 2 3 4]
-                [5 1 1 1]
-                [3 2 2 2]
-                [1 6 6 6]]]
+    (let [ eqs [[1 2 3 14] ;; roots 1 2 3
+                [5 1 1 10]
+                [3 2 2 13]
+                [1 6 6 31]]]
       (println (reorder eqs))))
 
   (testing "Reduction"
-    (let [[head eqs] [[5 1 1 1] '([1 6 6 6] [3 2 2 2] [1 2 3 4])]]
+    (let [[head eqs] [[5 1 1 10] '([1 6 6 31] [3 2 2 13] [1 2 3 14])] ]
       (println (row-reduce head eqs))))
 
-      
+  (testing "Pivot"
+    (let [ eqs [[1 2 3 14]
+                [5 1 1 10]
+                [3 2 2 13]
+                [1 6 6 31]]]
+      (println (pivot eqs))))
+
+  (testing "Solve"
+    (let [ eqs [[1 2 3 14]
+                [5 1 1 10]
+                [3 2 2 13]
+                [1 6 6 31]]
+           roots (solve eqs)]
+      (is (= '(1 2 3) roots))))
 )
+
