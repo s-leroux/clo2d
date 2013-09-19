a22f58895e462d6e14c98bb91a9fc3534aba5dd8
Sylvain Leroux <sylvain@chicoree.fr>
2013-09-19 22:35:16 +0200

    Experimental support for shape groups

---
+++ src/clo2d/shape.clj
@@ -0,0 +1,53 @@
+(ns clo2d.shape
+  (:use clo2d.infix))
+
+(defn updt-keywords
+ "Recursivly walk through a collection to prefix keywords by the given
+ string"
+ [ prefix coll ]
+ (let [f (fn [ x ]
+           (or 
+             (and (keyword? x) 
+                  (not (#{:=,:+,:-} x)) 
+                  (keyword (str prefix ":" (name x))))
+             (and (coll? x) (updt-keywords prefix x))
+             x)) ]
+    (map f coll)))
+
+(defn prefix-map
+ "prefix keys from a map by the given string"
+ [ prefix map ]
+ (let [ ks (keys map) 
+        f (fn [ x ]
+            (or 
+              (and (keyword? x) 
+                   (not (#{:=,:+,:-} x)) 
+                   (keyword (str prefix ":" (name x))))
+              (and (coll? x) (updt-keywords prefix x))
+              x)) ]
+   (into {} (for [[k v] map] [(f k) v]))))
+
+
+(def ^:const rectangular
+  (map parse-infix[ '( :bottom - :top = :height ) 
+                    '( 2 ( :bottom - :center-y ) = :height )
+                    '( :right - :left = :width )
+                    '( 2 ( :right - :center-x ) = :width ) ]))
+
+(def ^:const square
+  (concat rectangular (map parse-infix [
+                     '( :height = :width ) ])))
+
+(defmacro shape
+  [ name eqs ]
+  `(map #(prefix-map ~name %1) ~eqs))
+
+(defmacro group
+  [ & body ]
+  `(concat ~@body))
+
+(defmacro having
+  [ & constraints ]
+  `(map parse-infix '~constraints))
+
+
---
+++ test/clo2d/shape_test.clj
@@ -0,0 +1,18 @@
+(ns clo2d.shape-test
+  (:require [clojure.test :refer :all]
+            [clo2d.shape :refer :all])
+  (:import (java.io File)))
+
+(deftest updt-keywords-test
+  (testing "Updating keywords"
+    (doseq [[in out] [
+                 '( ( :a )  ( :x:a ) )
+                 '( ( :a :b )  ( :x:a :x:b ) )
+                 '( ( 1 :b )  ( 1 :x:b ) )
+                 '( ( :a ( :b :c) )  ( :x:a ( :x:b :x:c) ) )
+                 '( ( :a ( :b :+ :c) )  ( :x:a ( :x:b :+ :x:c) ) )
+                 '( ( :a ( :b :- :c) )  ( :x:a ( :x:b :- :x:c) ) )
+                 '( ( :a ( :b := :c) )  ( :x:a ( :x:b := :x:c) ) )
+                 '( ( :a ( :b + :c) )  ( :x:a ( :x:b + :x:c) ) )
+                     ]]
+      (is (= (updt-keywords "x" in) out)))))
