1d4f5b6e50e30cbdbce1507206f382a9bcba8d66
Sylvain Leroux <sylvain@chicoree.fr>
2013-09-19 16:52:40 +0200

    Basic support for parsing infix expressions

---
+++ src/clo2d/infix.clj
@@ -0,0 +1,63 @@
+(ns clo2d.infix
+  (:use clojure.set))
+
+(defn push
+  [stack v]
+  (cons v stack))
+
+(defn pop
+  [stack]
+  (let [[v & tail] stack ]
+    [v (or tail '())]))
+
+(defn product
+  [ map factor ]
+  (let [ks (keys map)]
+    (zipmap ks (for [k ks] (* factor (map k))))))
+
+(defn back-eval
+  [ values ops ]
+    (let [op (first ops)
+          value (first values)]
+      (case op
+        := (recur (cons (product value -1) (rest values)) 
+                  (cons :+ (rest ops)))
+        :+ (let [values (rest values)
+                 v2 (first values)]
+             (recur (cons (merge-with + value v2) (rest values))
+                    (rest ops)))
+
+        value)))
+
+
+(defn parse-infix
+  "Parse an infix (linear) equation"
+  [ eq ]
+  (loop [ terms eq
+          values '()
+          ops '() 
+          sign 1 ]
+    (let [ term (first terms)
+           tail (rest terms) ]
+      (cond
+        (not term)
+        (back-eval values ops)
+
+        (#{+,:+} term)
+        (recur (rest terms) values (cons :+ ops) 1)
+
+        (#{-,:-} term)
+        (recur (rest terms) values (cons :+ ops) -1)
+
+        (#{=,:=} term)
+        (recur (rest terms) values (cons := ops) 1)
+
+        (keyword? term)
+        (recur (cons sign terms) values ops 1)
+
+        (number? term)
+        (let [k (first tail) r (rest tail)]
+          (if (keyword? k)
+            (recur r (cons { k (* sign term) } values) ops 1)
+            (recur tail (cons { := (* sign term)} values) ops 1)))
+      ))))
---
+++ test/clo2d/infix_test.clj
@@ -0,0 +1,45 @@
+(ns clo2d.infix-test
+  (:require [clojure.test :refer :all]
+            [clo2d.infix :refer :all]
+            [clojure.java.io :refer :all])
+  (:import (java.io File)))
+
+(deftest base-functions-test
+  (testing "Product"
+    (let [ m {:a 1 :b 2 :c 3} ]
+      (is (= (product m 3) {:a 3 :b 6 :c 9})))))
+
+(deftest stack-test
+  (testing "value+keyword"
+    (let [s '()
+          s (push s {:a 3 })
+
+          [v s] (pop s)]
+      (is (= v {:a 3}))
+      (is (= s '()))))
+)
+
+(deftest infix-parser-test
+  (testing "back-eval add"
+   (let [ res (back-eval '({:a 3} {:b 1}) '(:+)) ]
+     (is (= res '{:a 3 :b 1})))
+  )
+
+  (testing "back-eval add same"
+   (let [ res (back-eval '({:a 3} {:a 1}) '(:+)) ]
+     (is (= res '{:a 4})))
+  )
+
+  (testing "back-eval add same"
+   (let [ res (back-eval '({:a 3} {:a 1 :b 2}) '(:+)) ]
+     (is (= res '{:a 4 :b 2})))
+  )
+
+  (testing "back-eval eq same"
+   (let [ res (back-eval '({:a 3} {:a 1 :b 2}) '(:=)) ]
+     (is (= res '{:a -2 :b 2})))
+  )
+
+  (testing "parser"
+    (let [eq [2 :x + 3 :y = 4 ] ]
+      (is (= (parse-infix eq) {:x 2 :y 3 := -4 })))))
